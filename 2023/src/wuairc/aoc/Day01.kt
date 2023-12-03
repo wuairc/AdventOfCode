0172562aaf68d66dcca5dba023bde1cc5ebb27a2
wuairc <wuairc@gmail.com>
2023-12-03 15:57:35 +0800

    complete day01

---
+++ 2023/src/wuairc/aoc/Day01.kt
@@ -0,0 +1,64 @@
+package wuairc.aoc
+
+import wuairc.aoc.util.Template
+
+fun main() {
+    Day01().solve()
+}
+
+class Day01 : Template(1) {
+    override fun part1(input: List<String>): Int {
+        return input.sumOf {
+            with(it.toCharArray()) {
+                first(Char::isDigit).digitToInt() * 10 + last(Char::isDigit).digitToInt()
+            }
+        }
+    }
+
+    override fun part2(input: List<String>): Int {
+        val digitMap: Map<String, Int> = mapOf(
+            "one" to 1,
+            "two" to 2,
+            "three" to 3,
+            "four" to 4,
+            "five" to 5,
+            "six" to 6,
+            "seven" to 7,
+            "eight" to 8,
+            "nine" to 9,
+            "1" to 1,
+            "2" to 2,
+            "3" to 3,
+            "4" to 4,
+            "5" to 5,
+            "6" to 6,
+            "7" to 7,
+            "8" to 8,
+            "9" to 9
+        )
+
+        val maxKeyLength = digitMap.keys.maxOf { it.length }
+
+        return input.sumOf {
+            val firstDigit: Int = it.windowedSequence(maxKeyLength, 1, true).mapNotNull { slice ->
+                for (entry in digitMap.entries) {
+                    if (slice.startsWith(entry.key)) {
+                        return@mapNotNull entry.value
+                    }
+                }
+                return@mapNotNull null
+            }.first()
+            val secondDigit: Int = it.windowedSequence(maxKeyLength, 1, true).mapNotNull { slice ->
+                for (entry in digitMap.entries) {
+                    if (slice.startsWith(entry.key)) {
+                        return@mapNotNull entry.value
+                    }
+                }
+                return@mapNotNull null
+            }.last()
+            firstDigit * 10 + secondDigit
+        }
+    }
+
+    override fun expectedTestOutput(): Pair<Int, Int> = 142 to 281
+}
--- 2023/src/wuairc/aoc/util/Utils.kt
+++ 2023/src/wuairc/aoc/util/Utils.kt
@@ -1,12 +1,16 @@
+package wuairc.aoc.util
+
 import java.math.BigInteger
 import java.security.MessageDigest
-import kotlin.io.path.Path
-import kotlin.io.path.readLines
 
 /**
  * Reads lines from the given input txt file.
  */
-fun readInput(name: String) = Path("src/$name.txt").readLines()
+fun readInput(name: String): List<String> {
+    return Template::class.java.getResourceAsStream("/input/$name.txt").use {
+        it!!.bufferedReader().readLines()
+    }
+}
 
 /**
  * Converts string to md5 hash.
